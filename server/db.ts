0b56e72e4ca7e388b4c41b46a05a338d46460cf5
Patricia Bárcena <45976722+Patri27@users.noreply.github.com>
2023-10-02 11:12:20 +0200

    changed end of line to lf

--- server/db.ts
+++ server/db.ts
@@ -1,17 +1,16 @@
 import mongoose, { ConnectOptions } from 'mongoose';
 
 async function connect() {
- try {
-   await mongoose
-          .connect('mongodb://127.0.0.1:27017/cocktail-companion', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-          } as ConnectOptions);
-   console.log('Connected to DB');
- } catch (err) {
-  console.log('database connection error')
- }
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/cocktail-companion', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    } as ConnectOptions);
+    console.log('Connected to DB');
+  } catch (err) {
+    console.log('database connection error');
+  }
 }
-connect()
+connect();
 
-module.exports = mongoose
+module.exports = mongoose;
